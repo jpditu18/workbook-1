d84d2c8b81357c896ac22293ba94ae6878051a7b
Jean Pierre Ditu <jditu5@gmail.com>
2025-04-10 11:26:26 -0500

    completed ShoppingList app

---
+++ Shopping-list/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,18 @@
+package com.pluralsight;
+
+public class ShoppingList {
+
+    public static void main(String[] args) {
+        System.out.println("Apple");
+        System.out.println("Tomato");
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Strawberry");
+        System.out.println("Sour Patch Watermelon");
+        System.out.println("Iphone 40");
+        System.out.println("GTA 6");
+        System.out.println("Gaming PC");
+        System.out.println("Avocado");
+    }
+
+}
